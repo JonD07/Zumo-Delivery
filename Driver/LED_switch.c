861c27fcb3e888c578746a8be63cd6bc7c4616ef
jnabholz <jacknabholz@gmail.com>
2021-04-25 14:08:32 -0600

    Added LED_switch.h and LED_switch.c, added the necessary functions, and coded them to pin PD5

---
+++ Driver/LED_switch.c
@@ -0,0 +1,2 @@
+#include LED_switch.h
+
---
+++ Driver/LED_switch.h
@@ -0,0 +1,7 @@
+#ifndef LED_SWITCH_H
+#define LED_SWITCH_H
+
+#include <avr/io.h>         // Board Specific pin definations
+#include <avr/interrupt.h>  // for interrupt service routine use
+
+#include <ctype.h>
